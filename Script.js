b502060147b099e8dfe67243fbcc73d623dcbb9c
cLubber1337 <supersoroko@gmail.com>
2022-10-26 18:41:07 +0300

    add traing 1

--- Script.js
+++ Script.js
@@ -1,6 +1,58 @@
-function functionWithArgs (a,b) {
-    console.log(a+b);
-   }
-   functionWithArgs (1,2)
-   functionWithArgs (7,9)
+/*function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
+*/
+/*
+let year = 2013;
 
+if (year == 2015) {
+  console.log("Year is 2015");
+} else if (year == 2014) {
+  console.log("Year is 2014");
+} else {
+  console.log("Year is not 2015/2014");
+}
+*/
+
+/* let accessAllowed;
+let age = 19;
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+console.log(accessAllowed);
+*/
+/*
+let age = prompt("How old are you?");
+let gender = prompt("what is your gender");
+let massage;
+
+if (age >= 18 && gender == "man") {
+  massage = "Hello Gentlemen";
+} else if (age >= 18 && gender == "woman") {
+  massage = " Hello Lady";
+} else {
+  massage = "Hello kid";
+  alert(massage);
+}*/
+/*
+let hour = 12;
+if (hour < 10 || hour > 18) {
+  console.log("office is closed");
+} else {
+  console.log("office is open");
+}
+/*
+let value = false;
+let result = !value;
+console.log(result);
+*/
+/*
+?? - борьба с undefined  и null
+let user;
+
+console.log(`Hello, ${user ?? "Anonim"}`);
+*/
